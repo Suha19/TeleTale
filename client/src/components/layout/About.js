562ceaed8ceac03c3e1d8f546ceb937d9dc8c126
Suha19 <47680905+Suha19@users.noreply.github.com>
2019-08-03 22:50:49 -0500

    deploy fail

--- client/src/components/layout/About.js
+++ client/src/components/layout/About.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 const About = () => {
   return (
-    <section className='landing'>
-      <div className='dark-overlay'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
-          <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
-          </p>
-        </div>
-      </div>
-    </section>
+    <p className='lead'>
+      If you usually have difficulty brainstorming storyline ideas, then this
+      application is perfect for you. Get out of the rut of writers’ block! A
+      little help from other creative minds can spark your imagination and
+      inspire your creative juices to flow. TeleTale is an application base on
+      The Telephone Game which we are all familiar with. The game can be played
+      by any age and in many situations. With TeleTale you can write and share
+      stories with people from all over the world. Players can read, add, and
+      vote on stories that they like. TeleTale is fun for the players, generates
+      laughs, and helps players socialize. TeleTale could also encourage active
+      reading and writing. For movie makers who are looking for some really cool
+      and new stories from real life people, we've got creative ideas for you.
+      This is an easy to use application, all you have to do is create your own
+      account and start interacting!
+    </p>
   );
 };
 export default About;
